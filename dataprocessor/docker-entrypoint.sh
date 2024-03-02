27824d7d97f9af778c445f19eaf0fb5b52025490
Geet Patel <geetpofficial@gmail.com>
2024-03-02 11:14:09 -0500

    Changing processor runner container

--- dataprocessor/docker-entrypoint.sh
+++ dataprocessor/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/flink/bin/flink run -m jobmanager:8081 -c org.example.Main /opt/dataprocessor.jar
+/opt/flink-1.18.1/bin/flink run -m jobmanager:8081 -c org.example.Main /opt/dataprocessor.jar
